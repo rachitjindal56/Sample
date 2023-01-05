d225caf03666b011fec5b6e41d5ce1df866a8839
rachitjindal56 <rachitjindal56@gmail.com>
2023-01-05 21:46:00 +0530

    Commit

--- app.py
+++ app.py
@@ -1,65 +1,77 @@
-# from flask import Flask,jsonify
-# from flask_restful import Api, Resource
-# from flask_httpauth import HTTPBasicAuth
-
-# app = Flask(__name__)
-# api = Api(app)
-# auth = HTTPBasicAuth()
-# USER_DATA = {
-#    "Username": "password",
-#    "sample": "sampling.io",
-#    "GPT-3": "OpenAI"
-# }
-
-# @auth.verify_password
-# def verify(username, password):
-#    if not (username and password):
-#        return False
-#    return USER_DATA.get(username) == password
-
-# class endpoint(Resource):
-#    @auth.login_required
-#    def get(self):
-#        return jsonify({"status":True})
-
-# api.add_resource(endpoint,"/endpoint")
-
-
-# if __name__=="__main__":
-#    app.run(port=5000,debug=True)
-
-# Using flask to make an api
-# import necessary libraries and functions
 from flask import Flask, jsonify, request
+import openai
+import os
+from dotenv import load_dotenv
+import random
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from scipy.special import softmax
+import numpy
 
-# creating a Flask app
+
 app = Flask(__name__)
+token = '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'
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+# os.environ['OPEN_API_KEY'] = "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"
 
-# on the terminal type: curl http://127.0.0.1:5000/
-# returns hello world when we use GET.
-# returns the data that we send when we use POST.
-@app.route('/', methods = ['GET', 'POST'])
+tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
+model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
+
+def sentiment_transformer(inp:str):
+    encoded_input = tokenizer(inp, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    
+    return numpy.argmax(scores)
+
+@app.route('/',methods=['POST'])
 def home():
-   if (request.method == 'GET'):
-      return jsonify({
-        "responses": [
-            {
-                "type": "text",
-                "delay": 1000,
-                "message": "Have a great day!"
-            }
-        ],
-      #   "attributes": {
-      #       "foo": "bar",
-      #       "baz": ""
-      #   }
-    })
+    if request.method == 'POST':
+        
+        response = ""
+        
+        response_sentiment = sentiment_transformer("Let's proceed to apply now")
+        print(response_sentiment,end="\n")
+        
+        if response_sentiment == 2:
+            response = "Proceed for further steps"
+        
+        else:
+            intents_sample = ["Inspire me","Tell something interesting","Don't feel interested", "Tell me something interesting about me",
+                            "Tell something interesting about my capabilities","Hmm, what to do?",
+                            "Hmm","Oh"]
+            
+            gpt_response = openai.Completion.create(
+                model="text-curie-001",
+                prompt=intents_sample[random.randint(0,len(intents_sample)-1)],
+                temperature=0.9,
+                max_tokens=150,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0.6,
+                stop=[" Human:", " AI:"])
+            
+            response = gpt_response['choices'][0]['text']
+        
+        print(response)
+        
+        return jsonify({
+            "responses": [
+                {
+                    "type": 'randomText',
+                    "messages": [response.lstrip().rstrip()]
+                }
+            ]
+        })
 
-@app.route('/<int:num>', methods = ['GET'])
-def disp(num):
+@app.route('/', methods = ['GET'])
+def disp():
 
-	return jsonify({'data': num**2})
+    # if (request.args.get('token') != token):
+    #     return '',401
+
+    return request.args.get('challenge')
 
 if __name__ == '__main__':
-
 	app.run(debug = True)
